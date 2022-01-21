30832ec043899fb0c944ba8ed80b79d2aaa64eec
Henrique Marques <dev6.gbmlogistica@outlook.com>
2022-01-21 14:48:12 -0300

    feat: project done

---
+++ server.js
@@ -0,0 +1,62 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/api/:date_string", (req, res) => {
+  const date_string = req.params.date_string;
+
+  let unix, utc;
+
+  if (new Date(parseInt(date_string)).toString() === "Invalid Date") {
+    res.json({
+      error: "Invalid Date",
+    });
+
+    return;
+  }
+
+  if (
+    new Date(parseInt(date_string)).getTime() === parseInt(date_string) &&
+    date_string.indexOf("-") === -1 &&
+    date_string.indexOf(" ") === -1
+  ) {
+    unix = parseInt(date_string);
+    utc = new Date(parseInt(date_string)).toUTCString();
+  } else {
+    unix = new Date(date_string).getTime();
+    utc = new Date(parseInt(unix)).toUTCString();
+  }
+
+  res.json({
+    unix,
+    utc,
+  });
+});
+
+app.get("/api/", (req, res) => {
+  res.json({
+    unix: new Date().valueOf(),
+    utc: new Date().toUTCString(),
+  });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
